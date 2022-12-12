150ca8c42ca00ea6254eadf63e7244aa2a5ec7d2
Jonathan <1439821144@qq.com>
2022-12-12 17:03:32 +0800

    style: rename composables

--- vite.config.ts
+++ vite.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
         '@vueuse/core',
         {
           '~/store': ['useUserStore'],
-          '~/hooks': ['useRouterPermission', 'useLoading'],
+          '~/composables': ['useRouterPermission', 'useLoading'],
         },
       ],
       dts: './src/types/auto-imports.d.ts',
